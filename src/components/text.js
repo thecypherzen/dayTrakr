b918ce030c664717ed0e720472106bac6c48db44
William Inyam <sw.inyam@outlook.com>
2024-07-04 22:00:45 +0000

    files(components): delete needless files

--- src/components/text.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const myarr = [false,true, false, false, true, true, false];
-const newarr = myarr.slice().sort((a,b) => a-b);
-const temp = myarr;
-console.log(newarr);
-console.log(myarr === newarr, myarr === temp);
