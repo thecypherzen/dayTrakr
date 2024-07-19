9613e9ab9fc51e9fb4c0d059131980676381fb04
William Inyam <sw.inyam@outlook.com>
2024-07-19 14:25:50 +0000

    deploy to render

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
+/*    server: {
 	port: 5000,
 	host: '0.0.0.0'
-    },
+    }, */
 })
