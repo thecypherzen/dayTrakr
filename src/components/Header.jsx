617670cc971bad3190969ec7c4ee9693f337f2d4
William Inyam <sw.inyam@outlook.com>
2024-07-19 15:16:08 +0000

    fix(asd:daytrakr): fix header component deploy error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from './header.module.css'
 
 export default function Header(){
     return (<>
-	    <header>
+	    <header className={ styles.header }>
 	    <div className="container">
 	    <h1> Day Trackr </h1>
 	    <p> keep accurate track of your day-to-day. </p>
